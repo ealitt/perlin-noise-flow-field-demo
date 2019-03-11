6a9849f8da217aa6f91cb54781577f3edccf6e79
Eammon <ealitt@gmail.com>
2019-03-11 11:41:35 -0400

    random points

--- js/sketch.js
+++ js/sketch.js
@@ -1,23 +1,27 @@
 var xoff = 0;
-var yoff = 0;
+var yoff = 100000;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   background(0);
 
-  let x = 0;
 }
 
 function draw(){
   // background(0);
 
-  var x = map(noise(xoff), 0, 1, 0, width);
-  var y = map(noise(yoff), 0, 1, 0, height);
+  for(var x = 0; x < width; x++){
+    stroke(255);
+    point(x, random(height));
+  }
 
-  xoff += 0.03;
-  yoff -= 0.04;
-
-  ellipse(x, y, 50, 50);
+  // var x = map(noise(xoff), 0, 1, 0, width);
+  // var y = map(noise(yoff), 0, 1, 0, height);
+  //
+  // xoff += 0.04;
+  // yoff -= 0.04;
+  //
+  // ellipse(x, y, 50, 50);
 
 }
 
